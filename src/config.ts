0490cd957affeac1fa7256cde5baff5f2c13c2ff
gaukharka <gaukhar.kashaganova@khresterion.com>
2025-09-09 08:57:56 +0200

    setting up cloudinary

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv'
+import cloudinary from 'cloudinary'
 
 dotenv.config({});
 
@@ -10,6 +11,10 @@ class Config{
     public SECRET_KEY_TWO: string|undefined;
     public CLIENT_URL: string|undefined;
     public REDIS_HOST: string|undefined;
+    public CLOUDE_NAME: string|undefined;
+    public CLOUD_API_KEY: string|undefined;
+    public CLOUD_API_SECRET: string|undefined;
+
 
     constructor(){
         this.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/socialconnect-backend'
@@ -18,7 +23,9 @@ class Config{
         this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || ''
         this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || ''
         this.CLIENT_URL = process.env.CLIENT_URL || ''
-
+        this.CLOUDE_NAME= process.env.CLOUDE_NAME || ''
+        this.CLOUD_API_KEY= process.env.CLOUD_API_KEY || ''
+        this.CLOUD_API_SECRET= process.env.CLOUD_API_SECRET || ''
     }
 
     public validateConfig(): void {
@@ -27,6 +34,14 @@ class Config{
                 throw new Error(`Configuration ${key} is undefined`)
             }
         }
+    }
+
+    public cloudinaryConfig(): void{
+      cloudinary.v2.config({
+        cloud_name: this.CLOUDE_NAME,
+        api_key: this.CLOUD_API_KEY,
+        api_secret: this.CLOUD_API_SECRET
+      })
     }
 };
 
